c34fde049dd59486092b195e0a36a41efd330d1e
Solrac <solrac@Carloss-MacBook-Pro.local>
2024-02-21 07:29:04 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['linked-dep'],
+  },
   build: {
     minify: true,
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/],
+    },
   },
 })
